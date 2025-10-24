026a8807fb48ebce667fcd090bbaf255f4303a90
graceconley23 <graceconley23@gmail.com>
2025-10-24 09:32:02 -0400

    defined some symbolic constraints and constants; testing of the display functions

--- wildfire.c
+++ wildfire.c
@@ -4,4 +4,21 @@
 // @author Grace Conley
 ////////////////////////////////////////////////////////////////////////
 
+#define _DEFAULT_SOURCE
 
+#include "display.h"
+#include "wildfire.h"
+
+int main() {
+
+	clear();
+	set_cur_pos(1,0);
+	put(BURNED);
+	set_cur_pos(2,0);
+	put(EMPTY);
+	set_cur_pos(2,1);
+	put(TREE);
+	set_cur_pos(2,2);
+	put(BURNING);
+	set_cur_pos(5,0);
+}
---
+++ wildfire.h
@@ -0,0 +1,12 @@
+#ifndef WILDFIRE_H
+#define WILDFIRE_H
+
+#define EMPTY ' '
+#define TREE 'Y'
+#define BURNING '*'
+#define BURNED '.'
+
+#define SEED 41
+#define DELAY 750000
+
+#endif
